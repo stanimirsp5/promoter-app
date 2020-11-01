26d4d6874c83e22d5f1e5b55b42377afd3bb2f7a
stanimir <stanimir.sp5@gmail.com>
2020-11-01 18:32:42 +0200

    add ignored files

---
+++ promotersapp/Scripts/Home/contactus.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="contact">
+        <h1>Contact us resr- Vue</h1>
+    </div>
+
+</template>
+<script>
+    export default {
+        name: "contact-component"
+    };
+</script>
