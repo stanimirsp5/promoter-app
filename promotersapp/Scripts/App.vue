f12ee563e58436e925367e02a26ed656a1178dfd
stanimir <stanimir.sp5@gmail.com>
2020-12-09 15:43:22 +0200

    add toolbar and footer

---
+++ promotersapp/Scripts/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <header-bar></header-bar>
+
+    <component v-bind:is="componentName"></component>
+
+    <footer-bar></footer-bar>
+  </v-app>
+</template>
+
+<script>
+import HomeComponent from "./Home/Home";
+import PromoterComponent from "./User/Promoter.vue";
+import LoginComponent from "./Login/Login.vue";
+
+import ContactComponent from "./Home/contactus.vue";
+import AboutComponent from "./Home/aboutus.vue";
+
+import headerBar from "./widgets/headerBar";
+import footerBar from "./widgets/footerBar";
+
+export default {
+  name: "price",
+  components: {
+    "home-component": HomeComponent,
+    "promoter-component": PromoterComponent,
+    "login-component": LoginComponent,
+
+    "contact-component": ContactComponent,
+    "about-component": AboutComponent,
+
+    "header-bar": headerBar,
+    "footer-bar": footerBar,
+  },
+  props: {
+    componentName: {
+      type: String,
+      default: "no-component",
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  methods: {},
+};
+</script>
+<style>
+</style>
--- promotersapp/Scripts/Home/home.vue
+++ promotersapp/Scripts/Home/home.vue
@@ -1,29 +1,5 @@
 <template>
   <div id="home1">
-    <v-card width="auto" flat tile>
-      <v-toolbar dense color="teal lighten-4">
-        <v-toolbar-title>testq17 Promotersapp</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-switch
-          @change="$vuetify.theme.dark = !$vuetify.theme.dark"
-          :label="`Dark mode ${$vuetify.theme.dark}`"
-        ></v-switch>
-
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </v-card>
     <v-container>
       <v-row dense class="px-4">
         <v-col cols="12">
